d462ead1ff2300060a79c07d67ceb5bfa0f63e99
sergiojoker11 <sergio.martin@flexys.co.uk>
2018-02-26 17:53:58 +0000

    10/10

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -1,28 +1,41 @@
 package calculator
 
 sealed abstract class Expr
+
 final case class Literal(v: Double) extends Expr
+
 final case class Ref(name: String) extends Expr
+
 final case class Plus(a: Expr, b: Expr) extends Expr
+
 final case class Minus(a: Expr, b: Expr) extends Expr
+
 final case class Times(a: Expr, b: Expr) extends Expr
+
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
   def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+                     namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    for {
+      (variable, expression) <- namedExpressions
+    } yield { (variable -> Signal(eval(expression(), namedExpressions))) }
   }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = expr match {
+    case Literal(v) => v
+    case Ref(name: String) => eval(getReferenceExpr(name, references), references - name)
+    case Plus(a: Expr, b: Expr) => eval(a, references) + eval(b, references)
+    case Minus(a: Expr, b: Expr) => eval(a, references) - eval(b, references)
+    case Times(a: Expr, b: Expr) => eval(a, references) * eval(b, references)
+    case Divide(a: Expr, b: Expr) => eval(a, references) / eval(b, references)
   }
 
   /** Get the Expr for a referenced variables.
-   *  If the variable is not known, returns a literal NaN.
-   */
+    * If the variable is not known, returns a literal NaN.
+    */
   private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
+                               references: Map[String, Signal[Expr]]) = {
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
